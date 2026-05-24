3c54f96c7f56e5951e94811ed1378d6ffa23107a
SUITE <i202319191@cibertec.edu.pe>
2026-05-24 12:41:01 -0500

    Implementacion de Listado Hecho

---
+++ src/main/java/Model/Orden_Soporte.java
@@ -0,0 +1,50 @@
+package Model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.DynamicInsert;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tbl_orden_soporte")
+@Getter
+@Setter
+@DynamicInsert
+
+public class Orden_Soporte {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "nro_orden")
+	private Integer nroOrden;
+	
+	@Column(name = "fecha_registro")
+	private LocalDate fechaRegistro;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_tecnico")
+	private Tecnico idTecnico;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_cliente")
+	private Cliente idCliente;
+	
+	@Column(name = "monto")
+	private Double monto;
+	
+	@Column(name = "detalle_incidencia")
+	private String detalleIncidencia;
+	
+}
